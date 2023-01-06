892bdcc2bb9e439346bb760c84f8b23385633dbe
ajay131 <ajay@innovation-hacks.ai>
2023-01-06 17:48:38 +0530

    All files

---
+++ src/eShopOnBlazor/Services/ICatalogService.cs
@@ -0,0 +1,17 @@
+﻿using eShopOnBlazor.Models;
+using eShopOnBlazor.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace eShopOnBlazor.Services;
+
+public interface ICatalogService : IDisposable
+{
+    CatalogItem FindCatalogItem(int id);
+    IEnumerable<CatalogBrand> GetCatalogBrands();
+    PaginatedItemsViewModel<CatalogItem> GetCatalogItemsPaginated(int pageSize, int pageIndex);
+    IEnumerable<CatalogType> GetCatalogTypes();
+    void CreateCatalogItem(CatalogItem catalogItem);
+    void UpdateCatalogItem(CatalogItem catalogItem);
+    void RemoveCatalogItem(CatalogItem catalogItem);
+}
